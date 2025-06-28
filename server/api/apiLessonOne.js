891b57fa4befd8fa969e63a7393c90e85e6a72da
YAtsevych <yatsevychya05@gmail.com>
2025-06-28 19:36:40 +0300

    d 12(12)

--- server/api/apiLessonOne.js
+++ server/api/apiLessonOne.js
@@ -10,18 +10,18 @@ apiLessonOne.get('/:slug/:slug2/:slug3', async (req, res) => {
     //Получить урок
     let lessonQuery = `SELECT
     lesson.*, rule.ruletext FROM lessons lesson LEFT JOIN rules rule ON lesson.idlesson = rule.connectiontolesson WHERE slug3 = $1;`
-    const [lessonRows] = await conn.query(lessonQuery, [slug3])
+    const { lessonRows } = await conn.query(lessonQuery, [slug3])
     if (lessonRows.length === 0) {
       return res.status(404).json({ error: 'Lesson not found' })
     }
 
     const lessonVocabularQuery = `SELECT  V.*, L.idlesson FROM lessons L JOIN lessonsvocabular LV ON L.idlesson = LV.idlesson JOIN dictionary V ON LV.idword = V.idword WHERE L.idlesson = $1;`
-    const [lessonVocabular] = await conn.query(lessonVocabularQuery, [
+    const { lessonVocabular } = await conn.query(lessonVocabularQuery, [
       lessonRows[0].idlesson,
     ])
 
     const tasksQuery = `SELECT * FROM tasks where lessonid = $1`
-    const [tasksRows] = await conn.query(tasksQuery, [lessonRows[0].idlesson])
+    const { tasksRows } = await conn.query(tasksQuery, [lessonRows[0].idlesson])
     tasksRows.forEach((task) => {
       try {
         task.options = JSON.parse(task.options)
